ea8f0fe960b4d13dd48ed997894d26b27695ee82
CS Crookes <140260264+cscrookes@users.noreply.github.com>
2025-11-03 07:07:21 -0800

    import

---
+++ CS5002_P2_Testing/src/impl/VendingMachine.java
@@ -0,0 +1,71 @@
+package impl;
+
+
+import exceptions.LaneCodeAlreadyInUseException;
+import exceptions.LaneCodeNotRegisteredException;
+import exceptions.ProductUnavailableException;
+import interfaces.IVendingMachineProduct;
+import interfaces.IVendingMachine;
+import interfaces.IProductRecord;
+
+/**
+ * This class represents a simple vending machine which can stock and sell products.
+ *
+ */
+public class VendingMachine implements IVendingMachine {
+
+    @Override
+    public void registerProduct(IVendingMachineProduct vendingMachineProduct) throws LaneCodeAlreadyInUseException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void unregisterProduct(IVendingMachineProduct vendingMachineProduct) throws LaneCodeNotRegisteredException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addItem(String laneCode) throws LaneCodeNotRegisteredException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void buyItem(String laneCode) throws ProductUnavailableException, LaneCodeNotRegisteredException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int getNumberOfProducts() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getTotalNumberOfItems() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfItems(String laneCode) throws LaneCodeNotRegisteredException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfSales(String laneCode) throws LaneCodeNotRegisteredException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public IVendingMachineProduct getMostPopular() throws LaneCodeNotRegisteredException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
